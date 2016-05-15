8780e1ccf099f3b3c00b53c24c149214c9d3de1f
star013 <thustar@sina.com>
2016-05-15 19:40:06 +0800

    debug for the so big a1, but have right trends

--- IO_K_Thm.m
+++ IO_K_Thm.m
@@ -12,12 +12,19 @@ ko = [3e6; 6e6];%Hz, corresponding to Q~2e8
 Q = 2*pi*c0./(lam0.*(ko+ke));
 
 a0 = sqrt(ke).*s0./((ko+ke)./2);
-a0 = a0.*[1;0];
+a0 = [1;0];
 kmm = 0;
-% B = 0.*B;
+sweep = 1e13:-1e11:-1e13;
 theta0 = [0; 0];
 a0 = a0.*[1*(cos(theta0(1))+1i*sin(theta0(1))); 0.5*(cos(theta0(2))+1i*sin(theta0(2)))];
 x0 = [real(a0(1)), imag(a0(1)), real(a0(2)), imag(a0(2))];
+P1 = zeros(length(sweep),1);
+for kw = 1:length(sweep)
+    dw = sweep(kw);
 func_final = fsolve(@(x) NL_K_Thm(x, kmm, delt, dw, ke, ko, B, s0), x0);
+if isreal(func_final(1))
 a = [func_final(1)+1i*func_final(2); func_final(3)+1i*func_final(4)];
 Pout = abs(a.*sqrt(ke)).^2;
+P1(kw) = Pout(1);
+end
+% figure; plot(sweep, P1);
