24cef949aebd7a7a52012e66fdd23cbfb891c8e5
star013 <thustar@sina.com>
2016-05-20 23:31:24 +0800

    finally find the bug! all about 2Pi, this version may be ready to go

--- IO_K_Thm.m
+++ IO_K_Thm.m
@@ -6,18 +6,18 @@ para;
 % tunable:
 dw = 0;
 s0 = sqrt(1e-3);    %input 1mW
-ke = [3e6; 6e6];
+ke = [3e6; 6e6].*(2*pi);    %rad/s
 
 % material para:
-ko = [3e6; 6e6];%Hz, corresponding to Q~2e8
-Q = 2*pi*c0./(lam0.*(ko+ke));
+ko = [3e6; 6e6].*(2*pi);%rad/s, corresponding to Q~2e8
+Q = 2*pi*c0./(lam0.*(ko+ke)./(2*pi));
 
 a0 = sqrt(ke).*s0./((ko+ke)./2);
 a0 = a0.*[1; 0];
 kmm = 0;
-sweep = -1e11:1e9:1e11;
+sweep = -1e10:1e8:1e10;
 %B = 1e-1.*B;
-theta0 = [pi/4; 0];
+theta0 = [0; 0];
 a0 = a0.*[1*(cos(theta0(1))+1i*sin(theta0(1))); 0.5*(cos(theta0(2))+1i*sin(theta0(2)))];
 x = [real(a0(1)), imag(a0(1)), real(a0(2)), imag(a0(2))];
 P1 = zeros(length(sweep),1);
@@ -34,10 +34,10 @@ for kw = 1:length(sweep)
     %     exitflag = -1;
     [func_final, fval, exitflag, output] = fsolve(@(x) NL_K_Thm(x, kmm, delt, dw, ke, ko, B, s0), x0, options);
     flag(kw) = exitflag;
-%     if exitflag <0
-%         x0 = 0.*x;
-%         [func_final, fval, exitflag, output] = fsolve(@(x) NL_K_Thm(x, kmm, delt, dw, ke, ko, B, s0), x0, options);
-%     end
+    if exitflag <0
+        x0 = 0.*x;
+        [func_final, fval, exitflag, output] = fsolve(@(x) NL_K_Thm(x, kmm, delt, dw, ke, ko, B, s0), x0, options);
+    end
     a = [func_final(1)+1i*func_final(2); func_final(3)+1i*func_final(4)];
     Pout = abs(a.*sqrt(ke)).^2;
     P1(kw) = Pout(1);
